d9c39fbb267dd2275a7fa7081fb27b056b820c24
UniQx <31258420+UniQxGit@users.noreply.github.com>
2018-03-15 12:17:05 -0700

    redraw ui elements

--- test.py
+++ test.py
@@ -11,18 +11,20 @@ pygame.display.set_caption("Uno Rabbids")
 #background image
 image = pygame.image.load("background_" + str(randint(1,10)) + ".jpg")
 
-
 screen = pygame.display.set_mode(image.get_rect().size)
 w, h = pygame.display.get_surface().get_size()
 
 SURF_BACKGROUND = image.convert()
-screen.blit(SURF_BACKGROUND ,(0,0))
 
-image = pygame.image.load("BorderOverlay.png")
-screen.blit(image ,(0,0))
 
-image = pygame.image.load("Deck_1.png")
-screen.blit(image ,(w * .05,h * .3))
+overlay = pygame.image.load("BorderOverlay.png").convert_alpha()
+
+
+image = pygame.image.load("Deck_1.png").convert_alpha()
+
+def MAX(left=0,right=0):
+	return left if (left>right) else right
+
 
 #card class
 class Card:
@@ -68,21 +70,29 @@ class Player:
 		self.isMain = isMain;
 
 	def redraw_hand(self):
-		startx = .2
+		minx = .23
+		maxx = .62
 		starty = .75
-		interval = (.5) / (len(self.hand))
+		startx = .4
+		interval = .09;#(.5) / (MAX(len(self.hand),1))
+		if(interval * (len(self.hand)-1) > (maxx-minx)):
+			interval *= (maxx-minx) / interval * (len(self.hand)-1)
+		startx -= (interval/2) * (len(self.hand)-1);
 		for i in range(len(self.hand)):
 			self.hand[i].rect.center = (w * startx,h * starty)
 			screen.blit(self.hand[i].image, (w * startx,h * starty))
 			startx += interval
+			pygame.display.update()
+		
 
 	def draw_card(self,count):
 		for i in range(count):
 			self.hand.append(deck[i])
 			deck[i].image = pygame.image.load("Cards/" + deck[i].name + ".png")
 			deck[i].rect = deck[i].image.get_rect()
+			#screen.blit(self.hand[i].image, (w * .5,h * .75))
 			deck.remove(deck[i])
-		self.redraw_hand()
+		#self.redraw_hand()
 		# image = pygame.image.load("Cards/card_3_4.png")
 		# screen.blit(image ,(w*.35,h*.75))
 		# image = pygame.image.load("Cards/card_2_6.png")
@@ -110,8 +120,7 @@ class Player:
 
 #turn based logic
 player1 = Player("player 1", True)
-player1.draw_card(5)
-player1.draw_card(1)
+#player1.draw_card(5)
 player2 = Player("player 2", False)
 
 
@@ -123,6 +132,10 @@ print (whose_turn.get_hand())
 
 #game loop
 while (True):
+	screen.blit(SURF_BACKGROUND ,(0,0))
+	screen.blit(overlay ,(0,0))
+	screen.blit(image ,(w * .05,h * .3))
+
 	for event in pygame.event.get():
 		if (event.type == QUIT): #pressing 'x' on the window
 			pygame.quit()
@@ -133,6 +146,10 @@ while (True):
 
 		if (event.type == KEYDOWN):
 			char_pressed = chr(event.key)
+			
+			key = pygame.key.get_pressed()
+			if (key[pygame.K_SPACE]):
+				player1.draw_card(1);
 
 			#check if move is legal according to whose turn it is
 			if (char_pressed in whose_turn.get_hand()):
@@ -151,6 +168,7 @@ while (True):
 				print (whose_turn.get_hand())
 			else:
 				print("You don't have that card!")
+	player1.redraw_hand()
 	pygame.display.update()
 
 
